f87a52f00c2527233818b55c5c24b2acfa780fd8
Alan Tan <alanwuha91@gmail.com>
2020-09-14 18:53:20 +0800

    Split array into multiple lines

--- autocommit.sh
+++ autocommit.sh
@@ -20,7 +20,10 @@ rnumber=$((RANDOM%17+3))
 echo "Creating ${rnumber} commits on $(date '+%a %d %b %Y')"
 
 # Declare associate array for ordinals
-declare -A ordinals=( [1]="First" [2]="Second" [3]="Third" [4]="Fourth" [5]="Fifth" [6]="Sixth" [7]="Seventh" [8]="Eighth" [9]="Ninth" [10]="Tenth" [11]="Eleventh" [12]="Twelveth" [13]="Thirteenth" [14]="Fourteenth" [15]="Fifteenth" [16]="Sixteenth" [17]="Seventeenth" [18]="Eighteenth" [19]="Nineteenth" [20]="Twentieth" )
+declare -A ordinals=( [1]="First" [2]="Second" [3]="Third" [4]="Fourth" [5]="Fifth"
+    [6]="Sixth" [7]="Seventh" [8]="Eighth" [9]="Ninth" [10]="Tenth" [11]="Eleventh"
+    [12]="Twelveth" [13]="Thirteenth" [14]="Fourteenth" [15]="Fifteenth" [16]="Sixteenth"
+    [17]="Seventeenth" [18]="Eighteenth" [19]="Nineteenth" [20]="Twentieth" )
 
 # README file name
 file="README.md"
