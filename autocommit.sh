4a68b7fbd2d6b55cfcc6b3d792a8e2393a0edee1
Alan Tan <alanwuha91@gmail.com>
2020-09-14 16:11:01 +0800

    Set working directory and format outputs

--- autocommit.sh
+++ autocommit.sh
@@ -10,9 +10,13 @@ echo "Starting autocommit"
 date
 echo
 
+# Directory
+cd /home/ubuntu/autocommit
+echo "Working directory: $(pwd)"
+
 # Generate a random number between 5 to 10
 rnumber=$((RANDOM%17+3))
-echo "Pushing ${rnumber} commits on $(date)"
+echo "Creating ${rnumber} commits on $(date '+%a %d %b %Y')"
 
 # Declare associate array for ordinals
 declare -A ordinals=( [1]="First" [2]="Second" [3]="Third" [4]="Fourth" [5]="Fifth" [6]="Sixth" [7]="Seventh" [8]="Eighth" [9]="Ninth" [10]="Tenth" [11]="Eleventh" [12]="Twelveth" [13]="Thirteenth" [14]="Fourteenth" [15]="Fifteenth" [16]="Sixteenth" [17]="Seventeenth" [18]="Eighteenth" [19]="Nineteenth" [20]="Twentieth" )
@@ -38,6 +42,7 @@ do
 done
 
 # Push commits to github repository
+echo "Pushing commits to $(git config --get remote.origin.url)"
 git push
 
 # Print end status message
