76d1ebc3cb0827e7a46432bb347e319583ffb35e
Alan Tan <alanwuha91@gmail.com>
2020-09-15 12:30:09 +0800

    Modify sed flag to delete all open attributes and format code

--- autocommit.sh
+++ autocommit.sh
@@ -15,21 +15,39 @@ directory="/home/ubuntu/autocommit"
 cd ${directory}
 echo "Working directory: $(pwd)"
 
-# Generate a random number between 5 to 10
-rnumber=$((RANDOM%17+3))
+# Generate a random number between 3 to 12
+rnumber=$((RANDOM%9+3))
 echo "Creating ${rnumber} commits on $(date '+%a %d %b %Y')"
 
 # Declare associate array for ordinals
-declare -A ordinals=( [1]="First" [2]="Second" [3]="Third" [4]="Fourth" [5]="Fifth"
-    [6]="Sixth" [7]="Seventh" [8]="Eighth" [9]="Ninth" [10]="Tenth" [11]="Eleventh"
-    [12]="Twelveth" [13]="Thirteenth" [14]="Fourteenth" [15]="Fifteenth" [16]="Sixteenth"
-    [17]="Seventeenth" [18]="Eighteenth" [19]="Nineteenth" [20]="Twentieth" )
+declare -A ordinals=(
+    [1]="First"
+    [2]="Second"
+    [3]="Third"
+    [4]="Fourth"
+    [5]="Fifth"
+    [6]="Sixth"
+    [7]="Seventh"
+    [8]="Eighth"
+    [9]="Ninth"
+    [10]="Tenth"
+    [11]="Eleventh"
+    [12]="Twelveth"
+    [13]="Thirteenth"
+    [14]="Fourteenth"
+    [15]="Fifteenth"
+    [16]="Sixteenth"
+    [17]="Seventeenth"
+    [18]="Eighteenth"
+    [19]="Nineteenth"
+    [20]="Twentieth"
+)
 
 # README file name
 file="README.md"
 
 # Remove open attribute from details tag
-sed -i -z 's/ open//' "${file}"
+sed -i "s/ open//" "${file}"
 
 # Create first commit using commit template
 output="## Commit log"
